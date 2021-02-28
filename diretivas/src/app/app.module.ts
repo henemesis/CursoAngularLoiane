03654aef9a87d19236b08a56f248a1f8600aee2f
ThiagoRM <henemesis@gmail.com>
2021-02-28 17:06:44 -0300

    Aulas: Cap 4 aula 4 - Diretiva ngFor
    Compreendendo o uso da Diretiva ngFor e o uso da variável local let

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { DiretivaForComponent } from './diretiva-for/diretiva-for.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
     DiretivaNgswitchComponent,
+    DiretivaForComponent,
   ],
   imports: [
     BrowserModule,
